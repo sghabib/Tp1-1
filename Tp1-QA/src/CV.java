c1b3d53bba9f4ebe1462541acba8dd26ce0c5cf5
Vaillancourt <0862745@cmontmorency.qc.ca>
2018-01-29 09:11:56 -0500

    Classe CV complétée

---
+++ Tp1-QA/src/CV.java
@@ -0,0 +1,34 @@
+
+public class CV {
+
+	private String nom;
+	private String prénom;
+	private String formation;
+	private int xp;
+	private String[] comp;
+	private String attente;
+	
+public CV (String nom, String prénom, String formation, int xp, String[] comp, String attente){
+		
+		this.nom = nom;
+		this.prénom = prénom;
+		this.formation = formation;
+		this.xp = xp;
+		System.arraycopy(this.comp, 0, comp, 0, comp.length);
+		this.attente= attente;
+		
+	}
+
+public void afficher(){
+	String temp="";
+	for(int i=0;i<this.comp.length;++i){
+		temp+=this.comp[i] + ", ";
+	}
+	System.out.println("Nom: " + this.nom + "\nPrénom: " + this.prénom + "\nFormation: " + this.formation + "\nAnnées d'expériences: " + this.xp + "\nCompétences: " + temp + "\nAttentes: " + this.attente );
+}
+	
+}
+
+	
+	
+	
